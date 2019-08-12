57c00b0c86503e9b89ca183532aeab2eda9c86a1
allan <1440806569@qq.com>
2019-08-12 11:05:41 +0800

    添加v0.0.5的apk地址

---
+++ flutter-weui-example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/allan/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/allan/Desktop/code/flutter/flutter_weui/flutter-weui-example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/allan/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
